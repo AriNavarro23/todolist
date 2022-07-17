f03b08e9c0abde5a13694797842eee76f7f544bc
Ariel Navarro <navarro.a.ariel@hotmail.com>
2022-07-17 16:02:00 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="toDoList">
+      <div className='toDoList-principal'>
+        <h1>My task</h1>
+        
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
